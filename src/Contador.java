0c4208b98d107f9ec223de25559ed48b5ec7baa8
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-07 15:04:29 -0300

    Atualização e melhora

--- src/Contador.java
+++ src/Contador.java
@@ -11,10 +11,10 @@ public class Contador {
         int parametroDois = terminal.nextInt();
 
         try {
-            // Chamando o método que realiza a contagem
+           
             contar(parametroUm, parametroDois);
         } catch (ParametrosInvalidosException exception) {
-            // Exibindo a mensagem de erro caso o segundo parâmetro seja menor
+           
             System.out.println(exception.getMessage());
         }
 
@@ -22,15 +22,15 @@ public class Contador {
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-        // Verifica se o primeiro parâmetro é maior que o segundo e lança a exceção
+        
         if (parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
 
-        // Calcula a quantidade de interações
+       
         int contagem = parametroDois - parametroUm;
 
-        // Realiza o loop para imprimir os números
+        
         for (int i = 1; i <= contagem; i++) {
             System.out.println("Imprimindo o número " + i);
         }
